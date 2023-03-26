779b6e3750d2a2bdfffd72bee5fbad0e87d69d2f
wlisses <wlissesfernando285@gmail.com>
2023-03-25 22:20:25 -0300

    implementado alguns estilos

--- src/pages/firstPage.js
+++ src/pages/firstPage.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
 import '../style/First.css';
 import arrImages from "../data/arrData";
+import Header from "./header";
 
 function FirstPage() {
 
+  function orderImages() {
+    const secondElement = document.querySelectorAll('.images-firtspage');
+    for (let index = 0; index < secondElement.length; index += 1) {
+      if (index % 2 === 0) {
+        secondElement[index].classList.add('order-first');
+      }       
+    }
+  }
+
+  useEffect(() => {
+    orderImages();
+  }, []);
+
   return (
-    <div>
+    <div className="main-firstpage">
+      <Header />
       <div className="main-cards">
         {arrImages.map((iten, index) => (
-          <div className="container-images">
-            <img src={ iten.image } alt={`bolsa ${ index }`} />
+          <div key={ index } className="container-images">
+            <img className="images-firtspage" src={ iten.image } alt={`bolsa ${ index }`} />
             <div>
               <h3>{iten.name}</h3>
               <h1>{`R$${iten.price}`}</h1>
---
+++ src/pages/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../style/header.css';
+
+function Header() {
+  return (
+    <div className="main-header">
+      Header
+    </div>
+  )
+};
+
+export default Header;
